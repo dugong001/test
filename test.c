1b3328d48aed88a2197d44496da9117a5727e5dc
urounamu <miyatiko@ezweb.ne.jp>
2018-06-29 13:20:16 +0900

    no message

--- test.c
+++ test.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(void){
+<<<<<<< Updated upstream
     printf("GitHub¥n");
+=======
+    printf("GitHub Test¥n");
+    printf("");
+>>>>>>> Stashed changes
 
     return 0;
 }
